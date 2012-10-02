36971aad0c91c044b246c05c1c377b413f04fe92
Jason Weathered <jason@jasoncodes.com>
2012-10-02 21:04:46 +1000

    Remove need for "def self.foo".

--- lib/github_watched/config.rb
+++ lib/github_watched/config.rb
@@ -1,12 +1,14 @@
 require 'shellwords'
 
 module GitHubWatched::Config
-  def self.git_config(name)
+  extend self
+
+  def git_config(name)
     value = `git config --get #{Shellwords.escape name}`.chomp
     value if $?.success?
   end
 
-  def self.github_username
+  def github_username
     git_config('github.user') or raise 'GitHub username not set. Use `git config --global github.user username` to set.'
   end
 end
